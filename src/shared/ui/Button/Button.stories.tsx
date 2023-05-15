da13a4345285d7cee823e04330fbac86aa78b06e
Do1phin <do1phin.name@gmail.com>
2023-05-15 17:39:57 +0300

    Update Button component

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -13,9 +13,6 @@ const meta: Meta<typeof Button> = {
       options: [...icons],
     },
   },
-  args: {
-    text: 'Primary Button',
-  },
   component: Button,
   title: 'UI-Kit/Button',
 };
@@ -23,140 +20,288 @@ export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {
+export const Default: Story = {
   args: {
-    state: 'primary',
+    text: 'Default button',
+    type: 'primary',
   },
-  render: (args) => <Button {...args}>Primary Button</Button>,
+  render: (args) => <Button {...args}>Default Button</Button>,
 };
 
 export const ButtonSizes: Story = {
-  args: {
-    state: 'primary',
-  },
   render: (args) => (
     <>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginBottom: '20px' }}>
-        <Button {...args} size={'small'}>
-          Primary Button
-        </Button>
-        <Button {...args} size={'normal'}>
-          Primary Button
-        </Button>
-        <Button {...args} size={'large'}>
-          Primary Button
-        </Button>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} type={'primary'} text={'Small Size'} size={'small'} />
+        <Button {...args} type={'primary'} text={'Normal Size'} size={'normal'} />
+        <Button {...args} type={'primary'} text={'Large Size'} size={'large'} />
       </div>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginBottom: '20px' }}>
-        <Button {...args} state={'secondary'} size={'small'}>
-          Secondary Button
-        </Button>
-        <Button {...args} state={'secondary'} size={'normal'}>
-          Secondary Button
-        </Button>
-        <Button {...args} state={'secondary'} size={'large'}>
-          Secondary Button
-        </Button>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} icon={'alien'} type={'primary'} text={'Small Size'} size={'small'} />
+        <Button {...args} icon={'alien'} type={'primary'} text={'Normal Size'} size={'normal'} />
+        <Button {...args} icon={'alien'} type={'primary'} text={'Large Size'} size={'large'} />
       </div>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginBottom: '20px' }}>
-        <Button {...args} state={'tertiary'} size={'small'}>
-          Tertiary Button
-        </Button>
-        <Button {...args} state={'tertiary'} size={'normal'}>
-          Tertiary Button
-        </Button>
-        <Button {...args} state={'tertiary'} size={'large'}>
-          Tertiary Button
-        </Button>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} icon={'alien'} type={'primary'} size={'small'} />
+        <Button {...args} icon={'alien'} type={'primary'} size={'normal'} />
+        <Button {...args} icon={'alien'} type={'primary'} size={'large'} />
       </div>
     </>
   ),
 };
 
-export const ButtonSizesWithLeftIcon: Story = {
-  args: {
-    icon: 'alien',
-    state: 'primary',
-  },
+export const ButtonTypes: Story = {
   render: (args) => (
-    <>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginBottom: '20px' }}>
-        <Button {...args} size={'small'}>
-          Primary Button
-        </Button>
-        <Button {...args} size={'normal'}>
-          Primary Button
-        </Button>
-        <Button {...args} size={'large'}>
-          Primary Button
-        </Button>
-      </div>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginBottom: '20px' }}>
-        <Button {...args} state={'secondary'} size={'small'}>
-          Secondary Button
-        </Button>
-        <Button {...args} state={'secondary'} size={'normal'}>
-          Secondary Button
-        </Button>
-        <Button {...args} state={'secondary'} size={'large'}>
-          Secondary Button
-        </Button>
-      </div>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginBottom: '20px' }}>
-        <Button {...args} state={'tertiary'} size={'small'}>
-          Tertiary Button
-        </Button>
-        <Button {...args} state={'tertiary'} size={'normal'}>
-          Tertiary Button
-        </Button>
-        <Button {...args} state={'tertiary'} size={'large'}>
-          Tertiary Button
-        </Button>
-      </div>
-    </>
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+      <Button {...args} type={'primary'} text={'Primary Icon'} size={'normal'} />
+      <Button {...args} type={'secondary'} text={'Secondary Icon'} size={'normal'} />
+      <Button {...args} type={'tertiary'} text={'Tertiary Icon'} size={'normal'} />
+    </div>
   ),
 };
 
-export const ButtonSizesWithRightIcon: Story = {
-  args: {
-    icon: 'alien',
-    iconPosition: 'right',
-    state: 'primary',
-  },
+export const ButtonStyles: Story = {
   render: (args) => (
     <>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginBottom: '20px' }}>
-        <Button {...args} size={'small'}>
-          Primary Button
-        </Button>
-        <Button {...args} size={'normal'}>
-          Primary Button
-        </Button>
-        <Button {...args} size={'large'}>
-          Primary Button
-        </Button>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} type={'primary'} text={'Info Primary'} style={'info'} />
+        <Button {...args} type={'primary'} text={'Info Primary'} style={'info'} />
+        <Button {...args} type={'primary'} text={'Info Primary'} style={'info'} />
+        <Button {...args} icon={'alien'} type={'primary'} text={'Info Primary'} style={'info'} />
+        <Button
+          {...args}
+          icon={'alien'}
+          iconPosition={'right'}
+          type={'primary'}
+          text={'Info Primary'}
+          style={'info'}
+        />
+        <Button {...args} icon={'alien'} type={'primary'} style={'info'} />
       </div>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginBottom: '20px' }}>
-        <Button {...args} state={'secondary'} size={'small'}>
-          Secondary Button
-        </Button>
-        <Button {...args} state={'secondary'} size={'normal'}>
-          Secondary Button
-        </Button>
-        <Button {...args} state={'secondary'} size={'large'}>
-          Secondary Button
-        </Button>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} type={'secondary'} text={'Info Secondary'} style={'info'} />
+        <Button {...args} type={'secondary'} text={'Info Secondary'} style={'info'} />
+        <Button {...args} type={'secondary'} text={'Info Secondary'} style={'info'} />
+        <Button
+          {...args}
+          icon={'alien'}
+          type={'secondary'}
+          text={'Info Secondary'}
+          style={'info'}
+        />
+        <Button
+          {...args}
+          icon={'alien'}
+          iconPosition={'right'}
+          type={'secondary'}
+          text={'Info Secondary'}
+          style={'info'}
+        />
+        <Button {...args} icon={'alien'} type={'secondary'} style={'info'} />
       </div>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginBottom: '20px' }}>
-        <Button {...args} state={'tertiary'} size={'small'}>
-          Tertiary Button
-        </Button>
-        <Button {...args} state={'tertiary'} size={'normal'}>
-          Tertiary Button
-        </Button>
-        <Button {...args} state={'tertiary'} size={'large'}>
-          Tertiary Button
-        </Button>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} type={'tertiary'} text={'Info Tertiary'} style={'info'} />
+        <Button {...args} type={'tertiary'} text={'Info Tertiary'} style={'info'} />
+        <Button {...args} type={'tertiary'} text={'Info Tertiary'} style={'info'} />
+        <Button {...args} icon={'alien'} type={'tertiary'} text={'Info Tertiary'} style={'info'} />
+        <Button
+          {...args}
+          icon={'alien'}
+          iconPosition={'right'}
+          type={'tertiary'}
+          text={'Info Tertiary'}
+          style={'info'}
+        />
+        <Button {...args} icon={'alien'} type={'tertiary'} style={'info'} />
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} type={'primary'} text={'Success Primary'} style={'success'} />
+        <Button {...args} type={'primary'} text={'Success Primary'} style={'success'} />
+        <Button {...args} type={'primary'} text={'Success Primary'} style={'success'} />
+        <Button
+          {...args}
+          icon={'alien'}
+          type={'primary'}
+          text={'Success Primary'}
+          style={'success'}
+        />
+        <Button
+          {...args}
+          icon={'alien'}
+          iconPosition={'right'}
+          type={'primary'}
+          text={'Success Primary'}
+          style={'success'}
+        />
+        <Button {...args} icon={'alien'} type={'primary'} style={'success'} />
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} type={'secondary'} text={'Success Secondary'} style={'success'} />
+        <Button {...args} type={'secondary'} text={'Success Secondary'} style={'success'} />
+        <Button {...args} type={'secondary'} text={'Success Secondary'} style={'success'} />
+        <Button
+          {...args}
+          icon={'alien'}
+          type={'secondary'}
+          text={'Success Secondary'}
+          style={'success'}
+        />
+        <Button
+          {...args}
+          icon={'alien'}
+          iconPosition={'right'}
+          type={'secondary'}
+          text={'Success Secondary'}
+          style={'success'}
+        />
+        <Button {...args} icon={'alien'} type={'secondary'} style={'success'} />
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} type={'tertiary'} text={'Success Tertiary'} style={'success'} />
+        <Button {...args} type={'tertiary'} text={'Success Tertiary'} style={'success'} />
+        <Button {...args} type={'tertiary'} text={'Success Tertiary'} style={'success'} />
+        <Button
+          {...args}
+          icon={'alien'}
+          type={'tertiary'}
+          text={'Success Tertiary'}
+          style={'success'}
+        />
+        <Button
+          {...args}
+          icon={'alien'}
+          iconPosition={'right'}
+          type={'tertiary'}
+          text={'Success Tertiary'}
+          style={'success'}
+        />
+        <Button {...args} icon={'alien'} type={'tertiary'} style={'success'} />
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} type={'primary'} text={'Danger Primary'} style={'danger'} />
+        <Button {...args} type={'primary'} text={'Danger Primary'} style={'danger'} />
+        <Button {...args} type={'primary'} text={'Danger Primary'} style={'danger'} />
+        <Button
+          {...args}
+          icon={'alien'}
+          type={'primary'}
+          text={'Danger Primary'}
+          style={'danger'}
+        />
+        <Button
+          {...args}
+          icon={'alien'}
+          iconPosition={'right'}
+          type={'primary'}
+          text={'Danger Primary'}
+          style={'danger'}
+        />
+        <Button {...args} icon={'alien'} type={'primary'} style={'danger'} />
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} type={'secondary'} text={'Danger Secondary'} style={'danger'} />
+        <Button {...args} type={'secondary'} text={'Danger Secondary'} style={'danger'} />
+        <Button {...args} type={'secondary'} text={'Danger Secondary'} style={'danger'} />
+        <Button
+          {...args}
+          icon={'alien'}
+          type={'secondary'}
+          text={'Danger Secondary'}
+          style={'danger'}
+        />
+        <Button
+          {...args}
+          icon={'alien'}
+          iconPosition={'right'}
+          type={'secondary'}
+          text={'Danger Secondary'}
+          style={'danger'}
+        />
+        <Button {...args} icon={'alien'} type={'secondary'} style={'danger'} />
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} type={'tertiary'} text={'Danger Tertiary'} style={'danger'} />
+        <Button {...args} type={'tertiary'} text={'Danger Tertiary'} style={'danger'} />
+        <Button {...args} type={'tertiary'} text={'Danger Tertiary'} style={'danger'} />
+        <Button
+          {...args}
+          icon={'alien'}
+          type={'tertiary'}
+          text={'Danger Tertiary'}
+          style={'danger'}
+        />
+        <Button
+          {...args}
+          icon={'alien'}
+          iconPosition={'right'}
+          type={'tertiary'}
+          text={'Danger Tertiary'}
+          style={'danger'}
+        />
+        <Button {...args} icon={'alien'} type={'tertiary'} style={'danger'} />
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} type={'primary'} text={'Warning Primary'} style={'warning'} />
+        <Button {...args} type={'primary'} text={'Warning Primary'} style={'warning'} />
+        <Button {...args} type={'primary'} text={'Warning Primary'} style={'warning'} />
+        <Button
+          {...args}
+          icon={'alien'}
+          type={'primary'}
+          text={'Warning Primary'}
+          style={'warning'}
+        />
+        <Button
+          {...args}
+          icon={'alien'}
+          iconPosition={'right'}
+          type={'primary'}
+          text={'Warning Primary'}
+          style={'warning'}
+        />
+        <Button {...args} icon={'alien'} type={'primary'} style={'warning'} />
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} type={'secondary'} text={'Warning Secondary'} style={'warning'} />
+        <Button {...args} type={'secondary'} text={'Warning Secondary'} style={'warning'} />
+        <Button {...args} type={'secondary'} text={'Warning Secondary'} style={'warning'} />
+        <Button
+          {...args}
+          icon={'alien'}
+          type={'secondary'}
+          text={'Warning Secondary'}
+          style={'warning'}
+        />
+        <Button
+          {...args}
+          icon={'alien'}
+          iconPosition={'right'}
+          type={'secondary'}
+          text={'Warning Secondary'}
+          style={'warning'}
+        />
+        <Button {...args} icon={'alien'} type={'secondary'} style={'warning'} />
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+        <Button {...args} type={'tertiary'} text={'Warning Tertiary'} style={'warning'} />
+        <Button {...args} type={'tertiary'} text={'Warning Tertiary'} style={'warning'} />
+        <Button {...args} type={'tertiary'} text={'Warning Tertiary'} style={'warning'} />
+        <Button
+          {...args}
+          icon={'alien'}
+          type={'tertiary'}
+          text={'Warning Tertiary'}
+          style={'warning'}
+        />
+        <Button
+          {...args}
+          icon={'alien'}
+          iconPosition={'right'}
+          type={'tertiary'}
+          text={'Warning Tertiary'}
+          style={'warning'}
+        />
+        <Button {...args} icon={'alien'} type={'tertiary'} style={'warning'} />
       </div>
     </>
   ),
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,9 +1,14 @@
-import type { ButtonStateTypes, IconPositionTypes, SizeTypes } from '../../types/componentTypes';
+import type {
+  IconPositionTypes,
+  SizeTypes,
+  StyleTypes,
+  TypeTypes,
+} from '../../types/componentTypes';
 import cx from 'classnames';
 import React, { FC } from 'react';
 
 import css from './Button.module.scss';
-import { Icon } from '../index';
+import icons from '../../icons/icons.svg';
 
 interface IButtonProps {
   disabled?: boolean;
@@ -11,26 +16,41 @@ interface IButtonProps {
   iconPosition?: IconPositionTypes;
   onClick: () => void;
   size?: SizeTypes;
-  state: ButtonStateTypes;
+  style?: StyleTypes;
   text?: string;
+  type?: TypeTypes;
 }
 
 const Button: FC<IButtonProps> = (props) => {
-  const { disabled, icon, iconPosition, onClick, size, state, text } = props;
+  const {
+    disabled = false,
+    icon,
+    iconPosition,
+    onClick,
+    size = 'normal',
+    style,
+    text,
+    type = 'primary',
+  } = props;
 
   return (
     <button
       className={cx(
         css.button,
-        css[state],
+        style && css[style],
+        size && css[size],
+        type && css[type],
         icon && css[iconPosition],
-        text ? css['text'] : css['icon'],
-        css[size],
+        text ? css.text : css['icon-only'],
       )}
       disabled={disabled}
       onClick={onClick}>
-      {icon && <Icon icon={icon} />}
-      {text}
+      {icon && (
+        <svg className={css.icon}>
+          <use xlinkHref={`${icons}#${icon}`} />
+        </svg>
+      )}
+      {text && <span className={css.text}>{text}</span>}
     </button>
   );
 };
