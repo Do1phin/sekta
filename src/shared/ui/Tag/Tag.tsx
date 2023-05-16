b27e2cbd92d82b0548d60f00c6fca4b1619622da
Do1phin <do1phin.name@gmail.com>
2023-05-16 14:52:59 +0300

    Update Tag component

--- src/shared/ui/Tag/Tag.tsx
+++ src/shared/ui/Tag/Tag.tsx
@@ -27,13 +27,13 @@ const Tag: FC<ITagProps> = (props) => {
   return (
     <div className={cx(css.tag, css[style], css[iconPosition])} disabled={disabled}>
       {icon && (
-        <picture className={cx(css.icon, css[size])}>
+        <picture className={cx(css.tag__icon, css[size])}>
           <svg>
             <use xlinkHref={`${icons}#${icon}`} />
           </svg>
         </picture>
       )}
-      {text && <span className={cx(css.text, css[size])}>{text}</span>}
+      {text && <span className={cx(css.tag__text, css[size])}>{text}</span>}
     </div>
   );
 };
