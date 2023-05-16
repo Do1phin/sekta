b92d84b402ba0b2c655dfedd71736da345fa9b19
Do1phin <do1phin.name@gmail.com>
2023-05-16 10:29:16 +0300

    Update Tag component

--- src/shared/ui/Tag/Tag.stories.tsx
+++ src/shared/ui/Tag/Tag.stories.tsx
@@ -6,13 +6,8 @@ import { Tag } from './Tag';
 
 const icons = ['alien'];
 
-const componentStates = ['default', 'primary', 'success', 'info', 'danger', 'warning'];
-
 const meta: Meta<typeof Tag> = {
   argTypes: {
-    args: {
-      text: 'my text',
-    },
     icon: {
       control: 'select',
       description: 'Tag icon name from svg-sprite',
@@ -22,15 +17,11 @@ const meta: Meta<typeof Tag> = {
       control: 'inline-radio',
       description: 'Tag value',
     },
-    state: {
+    style: {
       control: 'select',
       defaultValue: 'default',
       description: 'Tag state',
-      options: [...componentStates],
     },
-  },
-  args: {
-    state: 'default',
   },
   component: Tag,
   decorators: [withDesign],
@@ -42,220 +33,99 @@ type Story = StoryObj<typeof Tag>;
 
 export const Default: Story = {
   args: {
-    state: 'default',
+    style: 'default',
     text: 'Default Tag',
-  },
-  parameters: {
-    backgrounds: {
-      default: 'dark',
-    },
-    design: [
-      {
-        accessToken: process.env.STORYBOOK_FIGMA_ACCESS_TOKEN,
-        name: 'DESIGN',
-        type: 'figspec',
-        url: 'https://www.figma.com/file/l9Dr10AwifoYc9q5JkONKP/Transport-and-Logistics-Webflow-Website-Template-(Community)-(Copy)?node-id=1-4060&t=VT5SWV96RkD3Jk6F-4',
-      },
-      {
-        type: 'link',
-        url: 'https://www.figma.com/file/l9Dr10AwifoYc9q5JkONKP/Transport-and-Logistics-Webflow-Website-Template-(Community)-(Copy)?node-id=1-4060&t=VT5SWV96RkD3Jk6F-4',
-      },
-    ],
   },
   render: (args) => <Tag {...args}>Default Tag</Tag>,
 };
 
-export const TagStates: Story = {
-  parameters: {
-    backgrounds: {
-      default: 'dark',
-    },
-    design: [
-      {
-        accessToken: process.env.STORYBOOK_FIGMA_ACCESS_TOKEN,
-        name: 'DESIGN',
-        type: 'figspec',
-        url: 'https://www.figma.com/file/l9Dr10AwifoYc9q5JkONKP/Transport-and-Logistics-Webflow-Website-Template-(Community)-(Copy)?node-id=1-4060&t=VT5SWV96RkD3Jk6F-4',
-      },
-      {
-        type: 'link',
-        url: 'https://www.figma.com/file/l9Dr10AwifoYc9q5JkONKP/Transport-and-Logistics-Webflow-Website-Template-(Community)-(Copy)?node-id=1-4060&t=VT5SWV96RkD3Jk6F-4',
-      },
-    ],
-  },
-  render: (args) => (
-    <div style={{ display: 'flex', gap: '20px' }}>
-      <Tag {...args} state={'default'} text={'Default Tag'} />
-      <Tag {...args} state={'primary'} text={'Primary Tag'} />
-      <Tag {...args} state={'success'} text={'Success Tag'} />
-      <Tag {...args} state={'info'} text={'Info Tag'} />
-      <Tag {...args} state={'danger'} text={'Danger Tag'} />
-      <Tag {...args} state={'warning'} text={'Warning Tag'} />
-    </div>
-  ),
-};
-
-export const TagStatesWithLeftIcon: Story = {
-  args: {
-    icon: 'alien',
-  },
-  parameters: {
-    backgrounds: {
-      default: 'dark',
-    },
-    design: [
-      {
-        accessToken: process.env.STORYBOOK_FIGMA_ACCESS_TOKEN,
-        name: 'DESIGN',
-        type: 'figspec',
-        url: 'https://www.figma.com/file/l9Dr10AwifoYc9q5JkONKP/Transport-and-Logistics-Webflow-Website-Template-(Community)-(Copy)?node-id=1-4060&t=VT5SWV96RkD3Jk6F-4',
-      },
-      {
-        type: 'link',
-        url: 'https://www.figma.com/file/l9Dr10AwifoYc9q5JkONKP/Transport-and-Logistics-Webflow-Website-Template-(Community)-(Copy)?node-id=1-4060&t=VT5SWV96RkD3Jk6F-4',
-      },
-    ],
-  },
-  render: (args) => (
-    <div style={{ display: 'flex', gap: '20px' }}>
-      <Tag {...args} icon={'alien'} state={'default'} text={'Default Tag'} />
-      <Tag {...args} icon={'alien'} state={'primary'} text={'Primary Tag'} />
-      <Tag {...args} icon={'alien'} state={'success'} text={'Success Tag'} />
-      <Tag {...args} icon={'alien'} state={'info'} text={'Info Tag'} />
-      <Tag {...args} icon={'alien'} state={'danger'} text={'Danger Tag'} />
-      <Tag {...args} icon={'alien'} state={'warning'} text={'Warning Tag'} />
-    </div>
-  ),
-};
-
-export const TagStatesWithRightIcon: Story = {
-  args: {
-    icon: 'alien',
-    iconPosition: 'right',
-  },
-  parameters: {
-    backgrounds: {
-      default: 'dark',
-    },
-    design: [
-      {
-        accessToken: process.env.STORYBOOK_FIGMA_ACCESS_TOKEN,
-        name: 'DESIGN',
-        type: 'figspec',
-        url: 'https://www.figma.com/file/l9Dr10AwifoYc9q5JkONKP/Transport-and-Logistics-Webflow-Website-Template-(Community)-(Copy)?node-id=1-4060&t=VT5SWV96RkD3Jk6F-4',
-      },
-      {
-        type: 'link',
-        url: 'https://www.figma.com/file/l9Dr10AwifoYc9q5JkONKP/Transport-and-Logistics-Webflow-Website-Template-(Community)-(Copy)?node-id=1-4060&t=VT5SWV96RkD3Jk6F-4',
-      },
-    ],
-  },
-  render: (args) => (
-    <div style={{ display: 'flex', gap: '20px' }}>
-      <Tag {...args} state={'default'} text={'Default Tag'} />
-      <Tag {...args} state={'primary'} text={'Primary Tag'} />
-      <Tag {...args} state={'success'} text={'Success Tag'} />
-      <Tag {...args} state={'info'} text={'Info Tag'} />
-      <Tag {...args} state={'danger'} text={'Danger Tag'} />
-      <Tag {...args} state={'warning'} text={'Warning Tag'} />
-    </div>
-  ),
-};
-
 export const TagSizes: Story = {
-  parameters: {
-    backgrounds: {
-      default: 'dark',
-    },
-    design: [
-      {
-        accessToken: process.env.STORYBOOK_FIGMA_ACCESS_TOKEN,
-        name: 'DESIGN',
-        type: 'figspec',
-        url: 'https://www.figma.com/file/l9Dr10AwifoYc9q5JkONKP/Transport-and-Logistics-Webflow-Website-Template-(Community)-(Copy)?node-id=1-4060&t=VT5SWV96RkD3Jk6F-4',
-      },
-      {
-        type: 'link',
-        url: 'https://www.figma.com/file/l9Dr10AwifoYc9q5JkONKP/Transport-and-Logistics-Webflow-Website-Template-(Community)-(Copy)?node-id=1-4060&t=VT5SWV96RkD3Jk6F-4',
-      },
-    ],
-  },
   render: (args) => (
     <>
       <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginBottom: '20px' }}>
-        <Tag {...args} state={'default'} size={'small'} text={'Default Tag'} />
-        <Tag {...args} state={'primary'} size={'small'} text={'Primary Tag'} />
-        <Tag {...args} state={'success'} size={'small'} text={'Success Tag'} />
-        <Tag {...args} state={'info'} size={'small'} text={'Info Tag'} />
-        <Tag {...args} state={'danger'} size={'small'} text={'Danger Tag'} />
-        <Tag {...args} state={'warning'} size={'small'} text={'Warning Tag'} />
+        <Tag {...args} style={'default'} size={'small'} text={'Default Tag'} />
+        <Tag {...args} style={'primary'} size={'small'} text={'Primary Tag'} />
+        <Tag {...args} style={'success'} size={'small'} text={'Success Tag'} />
+        <Tag {...args} style={'info'} size={'small'} text={'Info Tag'} />
+        <Tag {...args} style={'danger'} size={'small'} text={'Danger Tag'} />
+        <Tag {...args} style={'warning'} size={'small'} text={'Warning Tag'} />
       </div>
       <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginBottom: '20px' }}>
-        <Tag {...args} state={'default'} size={'normal'} text={'Default Tag'} />
-        <Tag {...args} state={'primary'} size={'normal'} text={'Primary Tag'} />
-        <Tag {...args} state={'success'} size={'normal'} text={'Success Tag'} />
-        <Tag {...args} state={'info'} size={'normal'} text={'Info Tag'} />
-        <Tag {...args} state={'danger'} size={'normal'} text={'Danger Tag'} />
-        <Tag {...args} state={'warning'} size={'normal'} text={'Warning Tag'} />
+        <Tag {...args} style={'default'} size={'normal'} text={'Default Tag'} />
+        <Tag {...args} style={'primary'} size={'normal'} text={'Primary Tag'} />
+        <Tag {...args} style={'success'} size={'normal'} text={'Success Tag'} />
+        <Tag {...args} style={'info'} size={'normal'} text={'Info Tag'} />
+        <Tag {...args} style={'danger'} size={'normal'} text={'Danger Tag'} />
+        <Tag {...args} style={'warning'} size={'normal'} text={'Warning Tag'} />
       </div>
       <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
-        <Tag {...args} state={'default'} size={'large'} text={'Default Tag'} />
-        <Tag {...args} state={'primary'} size={'large'} text={'Primary Tag'} />
-        <Tag {...args} state={'success'} size={'large'} text={'Success Tag'} />
-        <Tag {...args} state={'info'} size={'large'} text={'Info Tag'} />
-        <Tag {...args} state={'danger'} size={'large'} text={'Danger Tag'} />
-        <Tag {...args} state={'warning'} size={'large'} text={'Warning Tag'} />
+        <Tag {...args} style={'default'} size={'large'} text={'Default Tag'} />
+        <Tag {...args} style={'primary'} size={'large'} text={'Primary Tag'} />
+        <Tag {...args} style={'success'} size={'large'} text={'Success Tag'} />
+        <Tag {...args} style={'info'} size={'large'} text={'Info Tag'} />
+        <Tag {...args} style={'danger'} size={'large'} text={'Danger Tag'} />
+        <Tag {...args} style={'warning'} size={'large'} text={'Warning Tag'} />
       </div>
     </>
   ),
 };
 
-export const TagSizesWithIcon: Story = {
-  args: {
-    icon: 'alien',
-  },
-  parameters: {
-    backgrounds: {
-      default: 'dark',
-    },
-    design: [
-      {
-        accessToken: process.env.STORYBOOK_FIGMA_ACCESS_TOKEN,
-        name: 'DESIGN',
-        type: 'figspec',
-        url: 'https://www.figma.com/file/l9Dr10AwifoYc9q5JkONKP/Transport-and-Logistics-Webflow-Website-Template-(Community)-(Copy)?node-id=1-4060&t=VT5SWV96RkD3Jk6F-4',
-      },
-      {
-        type: 'link',
-        url: 'https://www.figma.com/file/l9Dr10AwifoYc9q5JkONKP/Transport-and-Logistics-Webflow-Website-Template-(Community)-(Copy)?node-id=1-4060&t=VT5SWV96RkD3Jk6F-4',
-      },
-    ],
-  },
+export const TagStyles: Story = {
   render: (args) => (
     <>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginBottom: '20px' }}>
-        <Tag {...args} state={'default'} size={'small'} text={'Default Tag'} />
-        <Tag {...args} state={'primary'} size={'small'} text={'Primary Tag'} />
-        <Tag {...args} state={'success'} size={'small'} text={'Success Tag'} />
-        <Tag {...args} state={'info'} size={'small'} text={'Info Tag'} />
-        <Tag {...args} state={'danger'} size={'small'} text={'Danger Tag'} />
-        <Tag {...args} state={'warning'} size={'small'} text={'Warning Tag'} />
+      <div style={{ display: 'flex', gap: '20px', marginBottom: '20px' }}>
+        <Tag {...args} style={'default'} text={'Default Tag'} />
+        <Tag {...args} style={'default'} text={'Default Tag'} icon={'alien'} />
+        <Tag
+          {...args}
+          style={'default'}
+          text={'Default Tag'}
+          icon={'alien'}
+          iconPosition={'right'}
+        />
       </div>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginBottom: '20px' }}>
-        <Tag {...args} state={'default'} size={'normal'} text={'Default Tag'} />
-        <Tag {...args} state={'primary'} size={'normal'} text={'Primary Tag'} />
-        <Tag {...args} state={'success'} size={'normal'} text={'Success Tag'} />
-        <Tag {...args} state={'info'} size={'normal'} text={'Info Tag'} />
-        <Tag {...args} state={'danger'} size={'normal'} text={'Danger Tag'} />
-        <Tag {...args} state={'warning'} size={'normal'} text={'Warning Tag'} />
+      <div style={{ display: 'flex', gap: '20px', marginBottom: '20px' }}>
+        <Tag {...args} style={'primary'} text={'Primary Tag'} />
+        <Tag {...args} style={'primary'} text={'Primary Tag'} icon={'alien'} />
+        <Tag
+          {...args}
+          style={'primary'}
+          text={'Primary Tag'}
+          icon={'alien'}
+          iconPosition={'right'}
+        />
       </div>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
-        <Tag {...args} state={'default'} size={'large'} text={'Default Tag'} />
-        <Tag {...args} state={'primary'} size={'large'} text={'Primary Tag'} />
-        <Tag {...args} state={'success'} size={'large'} text={'Success Tag'} />
-        <Tag {...args} state={'info'} size={'large'} text={'Info Tag'} />
-        <Tag {...args} state={'danger'} size={'large'} text={'Danger Tag'} />
-        <Tag {...args} state={'warning'} size={'large'} text={'Warning Tag'} />
+      <div style={{ display: 'flex', gap: '20px', marginBottom: '20px' }}>
+        <Tag {...args} style={'success'} text={'Success Tag'} />
+        <Tag {...args} style={'success'} text={'Success Tag'} icon={'alien'} />
+        <Tag
+          {...args}
+          style={'success'}
+          text={'Success Tag'}
+          icon={'alien'}
+          iconPosition={'right'}
+        />
+      </div>
+      <div style={{ display: 'flex', gap: '20px', marginBottom: '20px' }}>
+        <Tag {...args} style={'info'} text={'Info Tag'} />
+        <Tag {...args} style={'info'} text={'Info Tag'} icon={'alien'} />
+        <Tag {...args} style={'info'} text={'Info Tag'} icon={'alien'} iconPosition={'right'} />
+      </div>
+      <div style={{ display: 'flex', gap: '20px', marginBottom: '20px' }}>
+        <Tag {...args} style={'danger'} text={'Danger Tag'} />
+        <Tag {...args} style={'danger'} text={'Danger Tag'} icon={'alien'} />
+        <Tag {...args} style={'danger'} text={'Danger Tag'} icon={'alien'} iconPosition={'right'} />
+      </div>
+      <div style={{ display: 'flex', gap: '20px' }}>
+        <Tag {...args} style={'warning'} text={'Warning Tag'} />
+        <Tag {...args} style={'warning'} text={'Warning Tag'} icon={'alien'} />
+        <Tag
+          {...args}
+          style={'warning'}
+          text={'Warning Tag'}
+          icon={'alien'}
+          iconPosition={'right'}
+        />
       </div>
     </>
   ),
--- src/shared/ui/Tag/Tag.tsx
+++ src/shared/ui/Tag/Tag.tsx
@@ -1,29 +1,39 @@
-import type { IconPositionTypes, StateTypes } from '../../types/componentTypes';
+import type { IconPositionTypes, SizeTypes, TagStyleTypes } from '../../types/componentTypes';
 import cx from 'classnames';
 import React, { FC } from 'react';
 
 import css from './Tag.module.scss';
-import { Icon, Text } from '../index';
+import icons from '../../icons/icons.svg';
 
 interface ITagProps {
-  fontSize: string;
+  disabled?: boolean;
   icon?: string;
   iconPosition?: IconPositionTypes;
-  state?: StateTypes;
+  size?: SizeTypes;
+  style?: TagStyleTypes;
   text: string;
 }
 
 const Tag: FC<ITagProps> = (props) => {
-  const { icon, iconPosition, state, fontSize, text } = props;
+  const {
+    disabled = 'false',
+    icon,
+    iconPosition = 'left',
+    style = 'default',
+    size = 'normal',
+    text,
+  } = props;
 
   return (
-    <div className={cx(css.tag, css[state], css[iconPosition || 'left'])}>
-      {icon && <Icon icon={icon} />}
-      {text && (
-        <Text fontSize={fontSize} state={props.state || 'default'}>
-          {text}
-        </Text>
+    <div className={cx(css.tag, css[style], css[iconPosition])} disabled={disabled}>
+      {icon && (
+        <picture className={cx(css.icon, css[size])}>
+          <svg>
+            <use xlinkHref={`${icons}#${icon}`} />
+          </svg>
+        </picture>
       )}
+      {text && <span className={cx(css.text, css[size])}>{text}</span>}
     </div>
   );
 };
