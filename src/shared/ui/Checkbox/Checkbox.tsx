bd34e2227b6c5a00ffee7a2def4ed9c5d66a97b1
Do1phin <do1phin.name@gmail.com>
2023-06-03 16:09:45 +0300

    Update Checkbox component

--- src/shared/ui/Checkbox/Checkbox.tsx
+++ src/shared/ui/Checkbox/Checkbox.tsx
@@ -2,28 +2,22 @@ import cx from 'classnames';
 import React, { FC } from 'react';
 
 import css from './Checkbox.module.scss';
-
-interface ICheckbox {
-  checked: boolean;
-  disabled?: boolean;
-  label: string;
-  onChange: () => void;
-  value: string;
-}
+import { ICheckbox } from './Checkbox.types';
 
 const Checkbox: FC<ICheckbox> = (props) => {
   const { checked = false, disabled = false, label, onChange, value } = props;
 
   return (
-    <label className={cx(css['custom-checkbox'], { checked: checked, disabled: disabled })}>
+    <label className={cx(css.checkbox, disabled && css.disabled, { checked: checked })}>
       <input
+        className={css.field}
         type='checkbox'
         checked={checked}
         disabled={disabled}
         onChange={onChange}
         value={value}
       />
-      <span>{label}</span>
+      <span className={css.label}>{label}</span>
     </label>
   );
 };
